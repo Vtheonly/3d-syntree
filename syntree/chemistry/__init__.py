3924b3c0956315306844aff84b3188fe3692c634
Mersel Fares <117942955+Vtheonly@users.noreply.github.com>
2026-09-23 17:21:44 +0100

    refactor: export reaction-family taxonomy

--- syntree/chemistry/__init__.py
+++ syntree/chemistry/__init__.py
@@ -1,6 +1,12 @@
 """Chemistry engine: reactions, catalogs, conformers, and validation."""
 
-from syntree.chemistry.reactions import ReactionEngine, REACTION_TEMPLATES, HANDLE_SMARTS
+from syntree.chemistry.reactions import (
+    ReactionEngine,
+    REACTION_TEMPLATES,
+    HANDLE_SMARTS,
+    REACTION_FAMILY_MEMBERS,
+    REACTION_FAMILY_NAMES,
+)
 from syntree.chemistry.catalog import SynthonCatalog
 from syntree.chemistry.conformer import ConformerEngine, VDW_RADII
 from syntree.chemistry.validator import ChemicalValidator
@@ -9,6 +15,8 @@ __all__ = [
     "ReactionEngine",
     "REACTION_TEMPLATES",
     "HANDLE_SMARTS",
+    "REACTION_FAMILY_MEMBERS",
+    "REACTION_FAMILY_NAMES",
     "SynthonCatalog",
     "ConformerEngine",
     "VDW_RADII",
